59e40b529fba9a20eb95e3b85d7ed4b208f20ca1
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2022-09-08 19:59:19 +0300

    fix old bugs

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,22 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    openModalMap: document.querySelector('[data-modal-map-open]'),
+    closeModalMap: document.querySelector('[data-modal-map-close]'),
+    modalMap: document.querySelector('[data-modal-map]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  refs.openModalMap.addEventListener('click', toggleModalMap);
+  refs.closeModalMap.addEventListener('click', toggleModalMap);
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleModalMap() {
+    refs.modalMap.classList.toggle('is-hidden');
   }
 })();
